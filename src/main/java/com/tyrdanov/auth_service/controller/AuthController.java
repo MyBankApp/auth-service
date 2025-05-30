7ed91445716ae12fd86ba6e53741ae8c529c443d
Алекс Тырданов <alekstyrdanov@Alekss-Mac-mini.local>
2025-05-30 22:58:34 +0300

    #16 feat: add getCurrentUser and validateToken functions for auth

--- src/main/java/com/tyrdanov/auth_service/controller/AuthController.java
+++ src/main/java/com/tyrdanov/auth_service/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.tyrdanov.auth_service.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +43,16 @@ public class AuthController {
     @PostMapping("/refresh")
     public AuthResponse refresh(@RequestBody RefreshRequest request) {
         return service.refresh(request);
+    }
+
+    @GetMapping("/me")
+    public UserDto getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return service.getCurrentUser(userDetails);
+    }
+
+    @GetMapping("/validate")
+    public boolean validateToken(@AuthenticationPrincipal UserDetails userDetails) {
+        return service.validate(userDetails);
     }
 
 }
